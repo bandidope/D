e869b62e0dc4a25f964008202ccb39020abf907e
YALLICO OWNER <yallico2024@gmail.com>
2026-06-17 21:12:02 -0500

    Actualizar _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -11,8 +11,8 @@ let icono = imagenes[Math.floor(Math.random() * imagenes.length)]
 */
 
 let botdata = global.db.data.settings[conn.user.jid] || {}
-global.iconimg1 = botdata.icon1 || 'https://files.catbox.moe/dpofx7.jpg'
-global.iconimg2 = botdata.icon2 || 'https://files.catbox.moe/h403wr.jpg'
+global.iconimg1 = botdata.icon1 || 'https://cdn.adoolab.xyz/dl/7e95df12.jpg'
+global.iconimg2 = botdata.icon2 || 'https://cdn.adoolab.xyz/dl/7e95df12.jpg'
 global.icono = pickRandom([global.iconimg1, global.iconimg2])
 
 global.rcanal = {
