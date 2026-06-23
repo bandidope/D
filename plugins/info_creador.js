1ab4b022cd6ca479784606ce68738e38899ea5f1
YALLICO OWNER <yallico2024@gmail.com>
2026-06-23 08:58:55 -0500

    Actualizar info_creador.js

--- plugins/info_creador.js
+++ plugins/info_creador.js
@@ -1,33 +1,73 @@
-import fetch from 'node-fetch';
+import PhoneNumber from 'awesome-phonenumber'
 
-let handler = async (m, { conn }) => {
-    const thumbUrl = `https://files.evogb.win/ddfwuH.jpg` 
-    const thumbBuffer = await fetch(thumbUrl).then(res => res.buffer());
+async function handler(m, { conn }) {
+  m.react('🌀')
 
-    let mensaje = `
-╭─❏ ✿ *Creador del Bot*
-┊ 👤 *Nombre:* Yallico
-┊ 🌐 *Fb:* Whois Yallico
-┊ 📱 *Instagram:* @whois.yallico
-┊ 📞 *WhatsApp:* wa.me/51936994155
-╰─❏ ✿`;
+  const contactos = [
+    {
+      numero: '51936994155',
+      nombre: 'Whois',
+      cargo: 'Dueño Principal',
+      nota: 'Creador del Bot',
+      correo: 'yallico2024@gmail.com',
+      region: 'Perú 🇵🇪',
+      web: ' https://Instagram.com/whois.yallico ',
+      biografia: await conn.fetchStatus('51936994155@s.whatsapp.net').then(res => res.status).catch(_ => 'Sin biografía')
+    }
+  ]
 
-    await conn.sendMessage(m.chat, {
-        text: mensaje,
-        contextInfo: {
-            externalAdReply: {
-                title: "Creador del Bot",
-                body: "Información de contacto",
-                thumbnail: thumbBuffer,
-                sourceUrl: `xvideos.com` 
-            }
-        }
-    }, { quoted: m })
+  const contactArray = contactos.map(c => [
+    c.numero,
+    c.nombre,
+    c.cargo,
+    c.nota,
+    c.correo,
+    c.region,
+    c.web,
+    c.biografia
+  ])
+
+  await sendContactArray(conn, m.chat, contactArray, m)
 }
 
-handler.help = ['creador']
+handler.help = ['owner', 'creador', 'creator']
 handler.tags = ['info']
-handler.command = ['creador', 'owner']
-handler.owner = false
+handler.command = ['owner', 'creator', 'creador', 'dueño']
 
 export default handler
+
+async function sendContactArray(conn, jid, data, quoted, options) {
+  if (!Array.isArray(data[0]) && typeof data[0] === 'string') data = [data]
+  let contacts = []
+  for (let [number, name, title, note, email, region, url, bio] of data) {
+    number = number.replace(/[^0-9]/g, '')
+    let vcard = `
+BEGIN:VCARD
+VERSION:3.0
+N:;${name.replace(/\n/g, '\\n')};;;
+FN:${name.replace(/\n/g, '\\n')}
+item.ORG:${title}
+item1.TEL;waid=${number}:${PhoneNumber('+' + number).getNumber('international')}
+item1.X-ABLabel:${note}
+item2.EMAIL;type=INTERNET:${email}
+item2.X-ABLabel:Correo
+item3.ADR:;;${region};;;;
+item3.X-ABADR:ac
+item3.X-ABLabel:Región
+item4.URL:${url}
+item4.X-ABLabel:Sitio Web
+item5.X-ABLabel:${bio}
+END:VCARD`.trim()
+    contacts.push({ vcard, displayName: name })
+  }
+
+  return await conn.sendMessage(jid, {
+    contacts: {
+      displayName: 'Propietarios del Bot',
+      contacts,
+    }
+  }, {
+    quoted,
+    ...options
+  })
+}
